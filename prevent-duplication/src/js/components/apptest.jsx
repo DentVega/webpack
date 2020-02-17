791df5663089776fba2fff5198e5698eb737e745
DentVega <dentvega6@gmail.com>
2020-02-17 18:17:54 -0400

    Evitar codigo duplicado

---
+++ prevent-duplication/src/js/components/apptest.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AppTest() {
+  return <div>Que linda aplicacion hecha en React.js Jsx</div>;
+}
+
+export default AppTest;
---
+++ prevent-duplication/src/js/contact.js
@@ -0,0 +1,7 @@
+import '../css/estilos.css';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/app';
+import AppTest from './components/apptest';
+
+render(<App />, document.getElementById('container'));
