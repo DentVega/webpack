66f38ab8203555b627c21bcae893a3f7ffc94e94
DentVega <dentvega6@gmail.com>
2020-02-17 23:02:11 -0400

    Dynamic imports

--- dll/dist/js/home.js
+++ dll/dist/js/home.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9bdce31c6832a767b0e0";
+/******/ 	var hotCurrentHash = "c0fed41a825216704be8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
